323c79c7bb791d2d77b09d99074f325a985dcbaf
Ken Bonny <bonny.ken+github@gmail.com>
2019-12-29 15:31:20 +0100

    create a single test

--- KenBonny.SpecFlowSetup/SomeTest.steps.cs
+++ KenBonny.SpecFlowSetup/SomeTest.steps.cs
@@ -1,4 +1,5 @@
 ﻿using TechTalk.SpecFlow;
+using Xunit;
 
 namespace KenBonny.SpecFlowSetup
 {
@@ -6,6 +7,22 @@ namespace KenBonny.SpecFlowSetup
     [Scope(Feature = "Some test")]
     public class SomeTestSteps
     {
-        
+        private int _first;
+        private int _second;
+        private int _result;
+
+        [Given(@"I have a number (.*)")]
+        public void GivenIHaveANumber(int first) => _first = first;
+
+        [Given(@"I have another number (.*)")]
+        public void GivenIHaveAnotherNumber(int second) => _second = second;
+
+        [When(@"I add them up")]
+        public void WhenIAddThemUp() => _result = Sum();
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int expected) => Assert.Equal(expected, _result);
+
+        private int Sum() => _first + _second;
     }
 }
