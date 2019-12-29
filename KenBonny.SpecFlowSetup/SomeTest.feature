feab0f7ded007c8c2289179c34b8537895325d97
Ken Bonny <bonny.ken+github@gmail.com>
2019-12-29 15:21:35 +0100

    empty setup

---
+++ KenBonny.SpecFlowSetup/SomeTest.feature
@@ -0,0 +1,7 @@
+﻿Feature: Some test
+  
+  Scenario: Just some test
+    Given I have a number 5
+    And I have another number 10
+    When I add them up
+    Then the result should be 15
---
+++ KenBonny.SpecFlowSetup/SomeTest.steps.cs
@@ -0,0 +1,11 @@
+﻿using TechTalk.SpecFlow;
+
+namespace KenBonny.SpecFlowSetup
+{
+    [Binding]
+    [Scope(Feature = "Some test")]
+    public class SomeTestSteps
+    {
+        
+    }
+}
